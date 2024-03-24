f63074f7598476be446a639b5dace53e2394b7ac
murali94-git <135966103+murali94-git@users.noreply.github.com>
2024-03-24 17:36:14 +0000

    my first terraform commit

---
+++ Terraform project/EC2 instance/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demoserver" {
+  ami = "ami-007020fd9c84e18c7"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraformserver"
+  }
+}
